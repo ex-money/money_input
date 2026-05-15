62ca0507183634a81c0895f8228efa0d7456d09e
Kip Cole <kipcole9@gmail.com>
2026-05-15 17:36:47 +1000

    Add json_polyfill when required

--- mix.exs
+++ mix.exs
@@ -116,7 +116,15 @@ defmodule MoneyInput.MixProject do
       {:bandit, "~> 1.5", optional: true},
       {:ex_doc, "~> 0.30", only: [:dev, :release], runtime: false},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false}
-    ]
+    ] ++ maybe_json_polyfill()
+  end
+
+  defp maybe_json_polyfill do
+    if Code.ensure_loaded?(:json) do
+      []
+    else
+      [{:json_polyfill, "~> 0.2 or ~> 1.0"}]
+    end
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
